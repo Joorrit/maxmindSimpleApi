032bcf2f55bf87853c33d414f8bf8276ffc7166a
Jorrit Krumrein <st183507@stud.uni-stuttgart.de>
2024-09-02 09:50:31 +0200

    added check if file exists

--- api/src/maxmind/lookupCity.ts
+++ api/src/maxmind/lookupCity.ts
@@ -1,11 +1,27 @@
 import { Reader } from '@maxmind/geoip2-node';
+import { promises as fs } from 'fs';
+
+async function fileExists(filePath: string) {
+    try {
+        await fs.access(filePath);
+        return true;
+    } catch {
+        return false;
+    }
+}
 
 const options = {
     watchForUpdates: true,
 };
 
-export function lookupCity(ip: string) {
-    return Reader.open('/usr/share/GeoIP/GeoLite2-City.mmdb', options).then(reader => {
-        return reader.city(ip);
-    });
+export async function lookupCity(ip: string) {
+    const filePath = '/usr/share/GeoIP/GeoLite2-City.mmdb';
+    const fileIsReady = await fileExists(filePath);
+
+    if (!fileIsReady) {
+        throw new Error(`File ${filePath} not found`);
+    }
+
+    const reader = await Reader.open(filePath, options);
+    return reader.city(ip);
 }
