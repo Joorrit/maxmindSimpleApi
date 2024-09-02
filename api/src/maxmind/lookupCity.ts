b6d47879fde32a72ed966460ba79b5dde7ad0495
Jorrit Krumrein <st183507@stud.uni-stuttgart.de>
2024-09-02 09:56:59 +0200

    chore: Update GeoIP file path and error handling

--- api/src/maxmind/lookupCity.ts
+++ api/src/maxmind/lookupCity.ts
@@ -1,27 +1,32 @@
 import { Reader } from '@maxmind/geoip2-node';
 import { promises as fs } from 'fs';
-
-async function fileExists(filePath: string) {
-    try {
-        await fs.access(filePath);
-        return true;
-    } catch {
-        return false;
-    }
-}
+import path from 'path';
 
 const options = {
     watchForUpdates: true,
 };
 
+async function fileExists(filePath: string) {
+    try {
+        await fs.access(filePath);
+        return true;
+    } catch (err) {
+        console.error(`File does not exist: ${filePath}`, err);
+        return false;
+    }
+}
+
 export async function lookupCity(ip: string) {
     const filePath = '/usr/share/GeoIP/GeoLite2-City.mmdb';
-    const fileIsReady = await fileExists(filePath);
+    const resolvedPath = path.resolve(filePath);
+    console.log(`Attempting to open file at: ${resolvedPath}`);
+
+    const fileIsReady = await fileExists(resolvedPath);
 
     if (!fileIsReady) {
-        throw new Error(`File ${filePath} not found`);
+        throw new Error(`File ${resolvedPath} not found`);
     }
 
-    const reader = await Reader.open(filePath, options);
+    const reader = await Reader.open(resolvedPath, options);
     return reader.city(ip);
 }
